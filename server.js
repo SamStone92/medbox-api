7f926cab572832927fc07540036897dd478d3f8c
Sam Stone <samstone920@googlemail.com>
2016-08-14 17:18:59 +0100

    huge changes to server.js

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-var express = require('express');
-var wine = require('./services/services');
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
-app.configure(function () {
-    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser());
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
-
-app.get('/services', wine.findAllInLocation);
-app.post('/services', wine.addService);
-
-app.listen(process.env.PORT || 5000);
-console.log('Listening on port 3000...');
