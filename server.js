087a22a3185b503b43c81fdd0aa4feee8f726721
Sam Stone <samstone920@googlemail.com>
2016-08-16 23:14:44 +0100

    changed for email

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.post("/users", function(req, res) {
 });
 
 app.get("/users/:email", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).findOne({ email: new ObjectID(req.params.email) }, function(err, doc) {
+  db.collection(CONTACTS_COLLECTION).findOne({ email: new ObjectID(req.params.id) }, function(err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to get contact");
     } else {
