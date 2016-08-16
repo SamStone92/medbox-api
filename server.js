7dff3e6b703a03382431f3c7b79f7446d5bed882
Sam Stone <samstone920@googlemail.com>
2016-08-16 20:29:30 +0100

    no message

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ var CONTACTS_COLLECTION = "users";
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
-    app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
