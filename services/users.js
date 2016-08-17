0c63725f58010dc9b64d5709ba46f40420f985cc
Sam Stone <samstone920@googlemail.com>
2016-08-18 00:05:03 +0100

    changed placing of user functions

--- services/users.js
+++ services/users.js
@@ -1,26 +1,41 @@
 
+var express = require("express");
+var path = require("path");
+var mongodb = require("mongodb");
+
 var CONTACTS_COLLECTION = "contacts";
 
-exports.addUser = function(req, res) {
-    console.log("got this far");
-  var newContact = req.body;
-  newContact.createDate = new Date();
+var db;
 
-  if (!(req.body.firstName || req.body.lastName)) {
-    console.log(err.message);
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
   }
 
-  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
-    if (err) {
-      console.log(err.message);
-    } else {
-      res.status(201).json(doc.ops[0]);
-    }
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
   });
-}
+});
 
 
-
+exports.getUser = function(req, res) {
+	var email = req.query.email;
+  	db.collection(CONTACTS_COLLECTION).findOne({ email: email}, function(err, doc) {
+    	if (err) {
+      		handleError(res, err.message, "Failed to get contact");
+    	} else {
+      		res.status(200).json(doc);
+    	}
+  	});
+};
 
 
 
