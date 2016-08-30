d51fbd6618661f2f370146e69669aea4f296e7e4
Sam Stone <samstone920@googlemail.com>
2016-08-30 19:59:15 +0100

    notification stuff sorted

--- server.js
+++ server.js
@@ -44,6 +44,21 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
+
+var options = { };
+var apnConnection = new apn.Connection(options);
+
+var myDevice = new apn.Device("C744BA1B-950C-4653-8E91-9620AB89A2EB");
+var note = new apn.Notification();
+
+note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+note.badge = 3;
+note.sound = "ping.aiff";
+note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+note.payload = {'messageFrom': 'Caroline'};
+
+apnConnection.pushNotification(note, myDevice);
+
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
     console.log(err);
