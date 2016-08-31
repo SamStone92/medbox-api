896c6c351fe14461a9fe5ad107f0ed2464bc884c
Sam Stone <samstone920@googlemail.com>
2016-08-31 22:55:46 +0100

    testing route

--- server.js
+++ server.js
@@ -132,6 +132,27 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
+app.use('/api', router);
+
+
+router.route('/bears')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+        
+        var bear = new User();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Bear created!' });
+        });
+        
+    });
+
 /*  THE PART FOR USERS */
 
 app.post("/users", passport.authenticate(['facebook-token']), 
