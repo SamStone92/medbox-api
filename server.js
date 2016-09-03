9b539f4f26ea78409c2616e84e7c106dbfacee36
Sam Stone <samstone920@googlemail.com>
2016-09-03 19:36:12 +0100

    no message

--- server.js
+++ server.js
@@ -366,17 +366,15 @@ app.delete("/users/:id", passport.authenticate(['facebook-token']),
 /*Notification schedule calls */
 
 app.get("/remindersForUser/:id", passport.authenticate(['facebook-token']), 
-        function (req, res) {
-            if (req.user){
-                db.collection(NOTIFICATION_SCHEDULE).findOne({ user: req.params.id }), function(err, doc) {
+          if (req.user){
+                db.collection(NOTIFICATION_SCHEDULE).findOne({ user: req.params.id }, function(err, doc) {
                   if (err) {
                     handleError(res, err.message, "Failed to get contact");
                   } else {
                     res.status(200).json(doc);
                   }
-                };
+                });
             } else {
-                // not authenticated. go away.
                 res.send(401)
             }
         }
