cf19830332a64e048c028e780461d1e1699870f5
Sam Stone <samstone920@googlemail.com>
2016-09-04 22:25:07 +0100

    no message

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ function notification_cron(){
                                 }
                               }
                              } else {
-                              console.log(now + " - " + user_schedule.reminders[i]);
+                              console.log(now + " - " + user_schedule.reminders[index]);
                              }
                             }
                           }
