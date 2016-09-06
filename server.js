7b7a3f6924822e2e60ad03236f663e8d14413092
Sam Stone <samstone920@googlemail.com>
2016-09-06 23:14:47 +0100

    no message

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ function notification_cron(){
 
                              if (compareDates(date, now)){
                               var index = i;
-                              for (var i = 0; i <= medication.length-1; i++) {
+                              for (var i = 1; i <= medication.length; i++) {
                                 if(index == medication[i].time){
                                   apnConnection.pushNotification(note, myDevice);
                                 } else {
