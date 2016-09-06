4064fafdba9d4dc20c26afa5d8d9f97efdfa7e91
Sam Stone <samstone920@googlemail.com>
2016-09-06 22:00:48 +0100

    no message

--- server.js
+++ server.js
@@ -153,7 +153,6 @@ function notification_cron(){
                                 }
                               }
 
-                              for
                              } else {
                              }
                             }
