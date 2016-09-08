a69b8b0f02673c6f2f083700ac0be9d96045e980
Sam Stone <samstone920@googlemail.com>
2016-09-08 20:23:11 +0100

    no message

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ function notification_cron(){
                              }
                             }
                           }
-                      });
+                      };
 
                   }
                 });
